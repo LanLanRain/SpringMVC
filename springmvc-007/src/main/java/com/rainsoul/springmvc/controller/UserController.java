323d0e04e24588872aa88eb431bec15bd0abe14a
SoulfulRain <RainSoul1120@gmail.com>
2024-05-11 14:36:58 +0800

    RESTFul风格

---
+++ springmvc-007/src/main/java/com/rainsoul/springmvc/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.rainsoul.springmvc.controller;
+
+import com.rainsoul.springmvc.bean.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String getAll() {
+        System.out.println("正在查询所有用户信息");
+        return "ok";
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    public String getById(@PathVariable("id") String id) {
+        System.out.println("正在根据用户id查询用户信息...，用户id是" + id);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String save(User user) {
+        System.out.println("正在保存用户信息...");
+        System.out.println(user);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.PUT)
+    public String modify(User user){
+        System.out.println("正在修改用户信息：" + user);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
+    public String del(@PathVariable("id") String id){
+        System.out.println("正在删除用户：" + id);
+        return "ok";
+    }
+
+}
